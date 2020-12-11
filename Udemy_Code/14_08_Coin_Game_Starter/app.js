18f58b9ed48e3a162f8f4aefb61635686d77142a
Brice Schultz <Briceschultz91@gmail.com>
2020-12-11 13:15:12 -0800

    Adding Udemy Folders

---
+++ Udemy_Code/14_08_Coin_Game_Starter/app.js
@@ -0,0 +1,20 @@
+function isTouching(a, b) {
+	const aRect = a.getBoundingClientRect();
+	const bRect = b.getBoundingClientRect();
+
+	return !(
+		aRect.top + aRect.height < bRect.top ||
+		aRect.top > bRect.top + bRect.height ||
+		aRect.left + aRect.width < bRect.left ||
+		aRect.left > bRect.left + bRect.width
+	);
+}
+
+const avatar = document.querySelector('#player');
+
+window.addEventListener('keyup', function(e) {
+	const currTop = extractPos(avatar.style.top);
+	avatar.style.top = `${currTop + 50}px`;
+});
+
+const extractPos = (pos) => parseInt(pos.slice(0, -2));
