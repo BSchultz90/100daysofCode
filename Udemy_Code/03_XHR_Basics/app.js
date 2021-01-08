1c9a978c7c0bc904ea524ee6fd5def344a78a2a5
Brice Schultz <Briceschutz91@gmail.com>
2021-01-07 21:48:43 -0700

    Final Commit of the night | Axios / Intro to Async Await

--- Udemy_Code/03_XHR_Basics/app.js
+++ Udemy_Code/03_XHR_Basics/app.js
@@ -1,18 +1,28 @@
-fetch("https://swapi.dev/api/planets/")
-  .then((response) => {
-    if (!response.result) {
-      throw new Error(`Something went Wrong: Status Code${response.status}`);
+// const fetchNextPlanets = (url = "https://swapi.dev/api/planets/") => {
+//   return axios.get(url);
+// };
+// const printPlanets = ({ data }) => {
+//   for (let planet of data.results) {
+//     console.log(planet.name);
+//   }
+//   return Promise.resolve(data.next);
+// };
 
-      return response.json();
-    }
-  })
-  .then((data) => {
-    for (let planet of data.results) {
-      console.log(planet);
-      console.log(planet.name);
-    }
-  })
-  .catch((err) => {
-    console.log("Something went wrong with Fetch");
-    console.log(err);
-  });
+// fetchNextPlanets()
+//   .then(printPlanets)
+//   .then(fetchNextPlanets)
+//   .then(printPlanets)
+//   .catch((err) => {
+//     console.log("ERRROR!!!", err);
+//   });
+
+async function getPlanets() {
+  try {
+    const res = await axios.get("https://swapi.dev/api/planedasfts/");
+    console.log(res);
+  } catch (e) {
+    console.log("IN CATCH!!!!", e);
+  }
+}
+
+getPlanets();
