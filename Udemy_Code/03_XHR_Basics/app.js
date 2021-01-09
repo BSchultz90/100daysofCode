f762c32a0fc5fc951415985f154f47d2965cd585
Brice Schultz <Briceschutz91@gmail.com>
2021-01-08 18:04:31 -0700

    cant spell planets I guess | typo

--- Udemy_Code/03_XHR_Basics/app.js
+++ Udemy_Code/03_XHR_Basics/app.js
@@ -18,7 +18,7 @@
 
 async function getPlanets() {
   try {
-    const res = await axios.get("https://swapi.dev/api/planes/");
+    const res = await axios.get("https://swapi.dev/api/planets/");
     console.log(res);
   } catch (e) {
     console.log("IN CATCH!!!!", e);
