ad8ad515ec64229b6fa8081290ed1a3242fefc63
Brice Schultz <Briceschutz91@gmail.com>
2021-01-13 14:18:06 -0700

    EOD Commit | Timer Project

---
+++ Projects/Timer Project/app.js
@@ -0,0 +1,13 @@
+// Defines the Timer Class
+class Timer {
+  constructor(durationInput, startButton, pauseButton) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+
+    this.startButton.addEventListener("click", this.start);
+  }
+  start() {
+    console.log("Time to start the timer!");
+  }
+}
