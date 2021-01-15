b7595ccbb9b7fe638b553db098695956dcd2a364
Brice Schultz <Briceschutz91@gmail.com>
2021-01-14 19:34:28 -0700

    EOD Commit | Timer Project Start Pause Buttons

--- Projects/Timer Project/app.js
+++ Projects/Timer Project/app.js
@@ -1,13 +1,25 @@
 // Defines the Timer Class
+
 class Timer {
   constructor(durationInput, startButton, pauseButton) {
     this.durationInput = durationInput;
     this.startButton = startButton;
     this.pauseButton = pauseButton;
 
-    this.startButton.addEventListener("click", this.start);
+    this.startButton.addEventListener("click", this.start.bind(this));
   }
-  start() {
-    console.log("Time to start the timer!");
-  }
+  start = () => {
+    this.tick();
+    setInterval(this.tick, 1000);
+  };
+
+  tick = () => {
+    console.log("tick");
+  };
 }
+
+const durationInput = document.querySelector("#duration");
+const startButton = document.querySelector("#start");
+const pauseButton = document.querySelector("#pause");
+
+const timer = new Timer(durationInput, startButton, pauseButton);
