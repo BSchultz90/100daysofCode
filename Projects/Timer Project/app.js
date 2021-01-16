a083e028e9cc4e4311fadd5efabeb601f6a02e79
Brice Schultz <Briceschultz91@gmail.com>
2021-01-15 22:40:28 -0700

    EOD commit | Timer Project | Basic Func Finished

--- Projects/Timer Project/app.js
+++ Projects/Timer Project/app.js
@@ -1,25 +1,42 @@
 // Defines the Timer Class
 
 class Timer {
-  constructor(durationInput, startButton, pauseButton) {
-    this.durationInput = durationInput;
-    this.startButton = startButton;
-    this.pauseButton = pauseButton;
+	constructor(durationInput, startButton, pauseButton) {
+		this.durationInput = durationInput;
+		this.startButton = startButton;
+		this.pauseButton = pauseButton;
 
-    this.startButton.addEventListener("click", this.start.bind(this));
-  }
-  start = () => {
-    this.tick();
-    setInterval(this.tick, 1000);
-  };
+		this.startButton.addEventListener('click', this.start);
+		this.pauseButton.addEventListener('click', this.pause);
+	}
+	start = () => {
+		this.tick();
+		this.timer = setInterval(this.tick, 1000);
+	};
 
-  tick = () => {
-    console.log("tick");
-  };
+	pause = () => {
+		clearInterval(this.timer);
+	};
+
+	get timeRemaining() {
+		return parseFloat(this.durationInput.value);
+	}
+
+	set timeRemaining(time) {
+		this.durationInput.value = time;
+	}
+
+	tick = () => {
+		if (this.timeRemaining <= 0) {
+			this.pause();
+		} else {
+			this.timeRemaining = this.timeRemaining - 1;
+		}
+	};
 }
 
-const durationInput = document.querySelector("#duration");
-const startButton = document.querySelector("#start");
-const pauseButton = document.querySelector("#pause");
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
 
 const timer = new Timer(durationInput, startButton, pauseButton);
