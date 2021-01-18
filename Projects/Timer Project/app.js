0e9cf3a8ad7c37ee738d82f4c9c7fd2d0a364ba1
Brice Schultz <Briceschultz91@gmail.com>
2021-01-17 21:01:07 -0700

    Finished Initial Timer / Moved Class to Separate File

--- Projects/Timer Project/app.js
+++ Projects/Timer Project/app.js
@@ -1,42 +1,17 @@
 // Defines the Timer Class
 
-class Timer {
-	constructor(durationInput, startButton, pauseButton) {
-		this.durationInput = durationInput;
-		this.startButton = startButton;
-		this.pauseButton = pauseButton;
-
-		this.startButton.addEventListener('click', this.start);
-		this.pauseButton.addEventListener('click', this.pause);
-	}
-	start = () => {
-		this.tick();
-		this.timer = setInterval(this.tick, 1000);
-	};
-
-	pause = () => {
-		clearInterval(this.timer);
-	};
-
-	get timeRemaining() {
-		return parseFloat(this.durationInput.value);
-	}
-
-	set timeRemaining(time) {
-		this.durationInput.value = time;
-	}
-
-	tick = () => {
-		if (this.timeRemaining <= 0) {
-			this.pause();
-		} else {
-			this.timeRemaining = this.timeRemaining - 1;
-		}
-	};
-}
-
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 
-const timer = new Timer(durationInput, startButton, pauseButton);
+const timer = new Timer(durationInput, startButton, pauseButton, {
+	onStart() {
+		console.log('Timer Started');
+	},
+	onTick() {
+		console.log('timer just ticked down');
+	},
+	onComplete() {
+		console.log('Time is Completed');
+	}
+});
