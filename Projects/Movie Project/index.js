ec0854d08248bb0813483860bf009cfd37f2b45d
Brice Schultz <Briceschutz91@gmail.com>
2021-01-21 19:29:22 -0700

    EOD Commmit

--- Projects/Movie Project/index.js
+++ Projects/Movie Project/index.js
@@ -1,1 +1,12 @@
-console.log('Hi there!');
+fetchData = async () => {
+  response = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      apikey: "c4a9c34d",
+      i: "tt0848228",
+    },
+  });
+  console.log(response.data);
+};
+
+const input = document.querySelector("input");
+input.addEventListener("input", () => {});
