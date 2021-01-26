84590ad2fb5bc2d9515a7f3c140ac3b218643f9b
Brice Schultz <Briceschutz91@gmail.com>
2021-01-26 14:15:41 -0700

    added images and h1s to api results

--- Projects/Movie Project/index.js
+++ Projects/Movie Project/index.js
@@ -1,17 +1,28 @@
-fetchData = async (searchTerm) => {
-  response = await axios.get("http://www.omdbapi.com/", {
+const fetchData = async (searchTerm) => {
+  const response = await axios.get("http://www.omdbapi.com/", {
     params: {
       apikey: "c4a9c34d",
       s: searchTerm,
     },
   });
-  console.log(response.data);
-};
 
-let timeoutId;
-onInput = (event) => {
-  fetchData(event.target.value);
+  return response.data.Search;
 };
 
 const input = document.querySelector("input");
+
+const onInput = async (event) => {
+  const movies = await fetchData(event.target.value);
+
+  for (let movie of movies) {
+    const div = document.createElement("div");
+
+    div.innerHTML = `
+      <img src="${movie.Poster}" />
+      <h1>${movie.Title}</h1>
+    `;
+
+    document.querySelector("#target").appendChild(div);
+  }
+};
 input.addEventListener("input", debounce(onInput, 500));
